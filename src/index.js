c03f071686129237ca31fedb184118c215986452
h1r0mu <asahina@sasase.ics.keio.ac.jp>
2021-01-25 00:51:01 +0900

    :rotating_light: Fixed Lint. #2

--- src/index.js
+++ src/index.js
@@ -1,81 +1,105 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {BrowserRouter,Route,Link} from 'react-router-dom'
-import './index.css';
-import Wheel from './wheel.js';
-
+import PropTypes from "prop-types";
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import "./index.css";
+import Wheel from "./wheel.js";
 
 const flavor_names = [
-            "野菜",
-            "酸味/発酵",
-            "フルーツ",
-            "花",
-            "甘味",
-            "ナッツココア",
-            "香辛料",
-            "焼き",
-            "その他"
-          ]
+  "野菜",
+  "酸味/発酵",
+  "フルーツ",
+  "花",
+  "甘味",
+  "ナッツココア",
+  "香辛料",
+  "焼き",
+  "その他",
+];
 
-const MainPage = (props) =>{
-	return(
-		<div className='Page{props.num}'> 
-			<h1>'coffee{props.num}'</h1>
-			<NextButton page={props.next} />
-		</div>
-	);
+const MainPage = (props) => {
+  return (
+    <div className="Page{props.num}">
+      <h1>coffee{props.num}</h1>
+      <NextButton page={props.next} />
+    </div>
+  );
 };
-const OtherPage = (props) =>{
-	return(
-		<div className='Page{props.num}'> 
-			<h1>'coffe{props.num}'</h1>
-			<BeforeButton page={props.before} />
-			<NextButton page={props.next} />
-		</div>
-	);
+MainPage.propTypes = {
+  num: PropTypes.number,
+  next: PropTypes.string,
 };
-const BeforeButton = (props) =>{
-		return(
-			<Link to = {props.page}>
-				    <button>{'Before'}</button>
-			</Link>
-		);
+const OtherPage = (props) => {
+  return (
+    <div className="Page{props.num}">
+      <h1>coffee{props.num}</h1>
+      <BeforeButton page={props.before} />
+      <NextButton page={props.next} />
+    </div>
+  );
 };
-const NextButton = (props) =>{
-		return(
-			<Link to = {props.page}>
-				    <button>{'Next'}</button>
-			</Link>
-		);
+OtherPage.propTypes = {
+  num: PropTypes.number,
+  before: PropTypes.string,
+  next: PropTypes.string,
+};
+const BeforeButton = (props) => {
+  return (
+    <Link to={props.page}>
+      <button>{"Before"}</button>
+    </Link>
+  );
+};
+BeforeButton.propTypes = {
+  page: PropTypes.string,
+};
+const NextButton = (props) => {
+  return (
+    <Link to={props.page}>
+      <button>{"Next"}</button>
+    </Link>
+  );
+};
+NextButton.propTypes = {
+  page: PropTypes.string,
 };
 
-const Pagenation = () => {
-	return(
-		<div>
-			<BrowserRouter>
-				<div>
-				   <Route path='/' exact render={() => 
-						   < MainPage num= {1} next='/page2'/> } />
-				   <Route path='/page2' render={() => 
-						   < OtherPage num= {2} next='/page3' before = '/' /> } />
-				   <Route path='/page3' render={() => 
-						   < OtherPage num= {3} next='/page4' before= '/page2' /> } />
-				   <Route path='/page4' render={() => 
-						   < OtherPage num= {4} next='/' before= '/page3' /> } />
-				</div>
-			</BrowserRouter>
-		</div>
-   );
+const Pagination = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route
+            path="/"
+            exact
+            render={() => <MainPage num={1} next="/page2" />}
+          />
+          <Route
+            path="/page2"
+            render={() => <OtherPage num={2} next="/page3" before="/" />}
+          />
+          <Route
+            path="/page3"
+            render={() => <OtherPage num={3} next="/page4" before="/page2" />}
+          />
+          <Route
+            path="/page4"
+            render={() => <OtherPage num={4} next="/" before="/page3" />}
+          />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    const flavors = flavor_names.map(name => {
+    const flavors = flavor_names.map((name) => {
       return {
         name: name,
-        selected: false
-      }
+        selected: false,
+      };
     });
     this.state = {
       flavors: flavors,
@@ -86,12 +110,10 @@ class App extends React.Component {
     return (
       <div className="service">
         <div className="service-board">
-          <Wheel
-            flavors={this.state.flavors}
-          />
+          <Wheel flavors={this.state.flavors} />
         </div>
-        <div className="pagenation">
-          <Pagenation/>
+        <div className="pagination">
+          <Pagination />
         </div>
       </div>
     );
