ca1b6d10cfd6424c1d001d74e1d3a4a8742b4d6c
Hinoronori Nakano <noribasket0429200001@yahoo.co.jp>
2021-02-10 14:58:53 +0900

    Fix css styles And Make start page (fix bug)

--- src/login.js
+++ src/login.js
@@ -4,7 +4,6 @@ import { ThemeProvider } from "@material-ui/styles";
 import { createMuiTheme } from "@material-ui/core/styles";
 import FileSystemNavigator from "./FileSystemNavigator.js";
 
-
 const theme = createMuiTheme({
   palette: {
     primary: {
@@ -16,59 +15,53 @@ const theme = createMuiTheme({
   },
 });
 const useStyles = makeStyles(() => ({
-		content:{
-				position: "relative",
-				minHeight: 300,
-				maxHeight: 800,
-				overflow: "hidden",
-				marginBottom: 500,
-		},
-		video: {
-				position: 'absolute',
-				width:'100%',
-				height: "100%",
-				top:'50%',
-				left:'50%',
-				objectFit:'cover',
-				transform:"translate(-50%,-50%)",
-				zIndex: -1,
-				filter: "grayscale(80%)",
-		},
-		h1:{
-				marginTop:100,
-				marginLeft:20,
-				fontSize:100,
-
-		},
-		start:{
-				marginTop:300,
-				marginLeft:680,
-				
-
-		},
+  content: {
+    position: "relative",
+    minHeight: 300,
+    maxHeight: 800,
+    overflow: "hidden",
+    marginBottom: 500,
+  },
+  video: {
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+    top: "50%",
+    left: "50%",
+    objectFit: "cover",
+    transform: "translate(-50%,-50%)",
+    zIndex: -1,
+    filter: "grayscale(80%)",
+  },
+  h1: {
+    marginTop: 100,
+    marginLeft: 20,
+    fontSize: 100,
+  },
+  start: {
+    marginTop: 300,
+    marginLeft: 680,
+  },
 }));
 
 export default function Login() {
   const classes = useStyles();
   return (
-				<div>
+    <div>
       <ThemeProvider theme={theme}>
-								<video 
-										src="/static/videos/coffee.mp4"
-										loop
-										muted
-										autoPlay
-										className={classes.video}
-								>
-								</video>
-								<h1 className={classes.h1}> Coffee Flavors</h1>
-								<div className={classes.start}>
-										<FileSystemNavigator/>
-								</div>
-								<FileSystemNavigator/>
+        <video
+          src="/static/videos/coffee.mp4"
+          loop
+          muted
+          autoPlay
+          className={classes.video}
+        ></video>
+        <h1 className={classes.h1}> Coffee Flavors</h1>
+        <div className={classes.start}>
+          <FileSystemNavigator />
+        </div>
+        <FileSystemNavigator />
       </ThemeProvider>
-				</div>
+    </div>
   );
 }
-
-
