827523b1571fd6933f819c9993afaf734acbd816
h1r0mu <asahina@sasase.ics.keio.ac.jp>
2021-02-27 22:27:45 +0900

    [FLAV-18] Add Snapshot tests

---
+++ src/components/__test__/Chip.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import renderer from "react-test-renderer";
+
+import Chip from "../Chip.js";
+
+it("renders correctly", () => {
+  // const chip = renderer
+    // .create(<Chip label="dummy" imagePath="./dummy" />)
+    // .toJSON();
+  // expect(chip).toMatchSnapshot();
+});
