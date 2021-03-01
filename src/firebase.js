b236a120c5ba845a72216c1f5b2beb99a503e2e5
Yuki Furuya <furuya@sasase.ics.keio.ac.jp>
2021-03-01 22:02:58 +0900

    [FLAV-41] Implemented logout function.

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,16 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_APIKEY,
-  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  apiKey: "AIzaSyAsVpb8we6Mhl0n-GWhLqQnjlLCFNiHKhA",
+  authDomain: "coffee-flavors-5c147.firebaseapp.com",
   databaseURL: process.env.REACT_APP_DATABASEURL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  projectId: "coffee-flavors-5c147",
+  storageBucket: "coffee-flavors-5c147.appspot.com",
+  messagingSenderId: "1027000250984",
+  appId: "1:1027000250984:web:6d454a177969df3ffe81dd",
+
+  // REACT_APP_MEASUREMENT_ID= "G-XVQ1ZL0KXW"
+  // REACT_APP_MAIL_URL=http://localhost:3000/
 };
 
 firebase.initializeApp(firebaseConfig);
