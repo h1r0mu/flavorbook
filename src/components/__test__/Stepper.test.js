55f4f697289fbce51aae96cdd902ad8cf13d369c
h1r0mu <asahina@sasase.ics.keio.ac.jp>
2021-03-05 20:42:02 +0900

    :arrow_up: Update yarn.lock

--- src/components/__test__/Stepper.test.js
+++ src/components/__test__/Stepper.test.js
@@ -1,14 +1,15 @@
 import React from "react";
-<<<<<<< HEAD
 import renderer from "react-test-renderer";
 
 import Stepper from "../Stepper.js";
 
-it('renders correctly', () => {
-  const stepper = renderer.create(
-    <Stepper level={0}>
-    <div>dummy</div>
-    </Stepper>
-  ).toJSON();
+it("renders correctly", () => {
+  const stepper = renderer
+    .create(
+      <Stepper level={0}>
+        <div>dummy</div>
+      </Stepper>
+    )
+    .toJSON();
   expect(stepper).toMatchSnapshot();
 });
