64de3fdfe9f521da6fda48d3958f949303120cc4
h1r0mu <asahina@sasase.ics.keio.ac.jp>
2021-03-24 00:49:13 +0900

    :fire: Removed LocalStorage middleware

--- src/reducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import historiesReducer from "./components/histories/historiesSlice";
-
-const rootReducer = combineReducers({
-  histories: historiesReducer,
-});
-
-export default rootReducer;
