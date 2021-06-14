96600caa78ff81a558b43f13ab961b7c3b268bbf
Yuki Furuya <furuya@sasase.ics.keio.ac.jp>
2021-06-15 08:34:41 +0900

    Refactor Page.js [FLAV-120]

--- src/features/myBeans2/Page.js
+++ src/features/myBeans2/Page.js
@@ -7,17 +7,19 @@ import { makeStyles } from "@material-ui/core/styles";
 import SearchForm from "./SearchForm";
 import Cards from "./Cards";
 import { beanIdFilterChanged } from "./beansFiltersSlice";
+import {Link} from "react-router-dom";
 
 const useStyles = makeStyles(() => ({
   root: {},
   cards: {},
   chips: {
     display: "flex",
-    maxWidth: 1000,
+    flexWrap: "wrap",
   },
   chipButton: {
     display: "flex",
     marginLeft: "auto",
+    flexWrap: "wrap",
   },
 }));
 
@@ -26,59 +28,77 @@ export const Page = () => {
 
   const classes = useStyles();
   const [key] = useState([""]);
+  const [editable, setEditable] = useState(false);
+  const editButtonLabel = !editable ? "Edit beans" : "Done";
+
+  const handleClick = () => {
+    setEditable(!editable);
+  };
 
   const onColorChange = (color) => {
     return dispatch(beanIdFilterChanged(color, "added"));
   };
 
-  // const addKey = (str) => {
-  //   return setKey(Array.from(new Set([...key, str])));
-  // };
-
-  const setKeyWords = (key) => {
+  const setCards = (key) => {
     return <Cards val={key} />;
   };
 
+  const setChips = (key) => {
+    const chipItems = key.map((word) => {
+      return word != " " ? (
+        <Chips
+          name={word}
+          pattern="Close"
+          color="secondry"
+          key={word}
+          onClick={() => removeChips(key, word)}
+        />
+      ) : (
+        <> </>
+      );
+    });
+    return <div className={classes.chips}>{chipItems}</div>;
+  };
+
+  const removeChips = (list, remove_word) => {
+    console.log(list, remove_word)
+    // setKey(list.filter((word) => word !== remove_word));
+  };
+
   useEffect(() => {
-    setKeyWords(key);
+    setCards(key);
+    setChips(key);
   }, [key]);
 
   return (
     <div className={classes.root}>
       <Grid container>
-        <Grid item xs={3}></Grid>
-        <Grid item xs={10}>
-          <div className={classes.chips}>
-            <Chips
-              name="CREATE"
-              pattern="Create"
-              color="primary"
-              className={classes.chipButton}
-            />
-            <Chips
-              name="EDIT"
-              pattern="Edit"
-              color="secondry"
-              className={classes.chipButton}
-            />
-          </div>
-        </Grid>
-      </Grid>
-      <Grid container>
+        <Grid item xs={1}></Grid>
         <Grid item xs={3}>
           <SearchForm name="Flavor" addKey={onColorChange} />
           <SearchForm name="Country" addKey={onColorChange} />
           <SearchForm name="Shop" addKey={onColorChange} />
           <SearchForm name="Roast" addKey={onColorChange} />
         </Grid>
-        <Grid item xs={9}>
-          <div className={classes.cards}>
-            <p>{key}</p>
-            <div>{setKeyWords(key)}</div>
+        <Grid item xs={7}>
+          <div>{setChips(key)}</div>
+          <div className={classes.chips}>
+            <Link to="/selection">
+              <Chips name="Register beans" pattern="Create" color="primary" />
+            </Link>
+            <Chips
+                name={editButtonLabel}
+                pattern="Edit"
+                color="secondry"
+                onClick={handleClick}
+            />
+          </div>
+          <div className={classes.chips}>
             <BeanList />
           </div>
         </Grid>
       </Grid>
+      <Grid item xs={1}></Grid>
     </div>
   );
 };
